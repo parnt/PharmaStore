5649a8e3cb1d064d3a4c42bfa7969293995ee001
Kamil Kucharczyk <kamil.kucharczyk@onexgroup.pl>
2019-04-09 11:56:15 +0200

    Add last view for add new medicine

--- PharmaStore/PharmaStoreWeb/Controllers/HomeController.cs
+++ PharmaStore/PharmaStoreWeb/Controllers/HomeController.cs
@@ -13,7 +13,9 @@ namespace PharmaStoreWeb.Controllers
 {
     using Microsoft.AspNetCore.Http;
     using Models.Medicines;
+    using Newtonsoft.Json;
     using PharmaStoreWeb.Models.Core;
+    using System.Text;
 
     public class HomeController : Controller
     {
@@ -78,6 +80,75 @@ namespace PharmaStoreWeb.Controllers
             catch
             {
                 return Error();
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddMedicine()
+        {
+            string urlBuild = $"{url}/MedicineTypes";
+
+            try
+            {
+                HttpResponseMessage response = await new HttpClient().GetAsync(urlBuild);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    var model = await response.Content.ReadAsAsync<Result<IEnumerable<MedicineType>>>();
+                    var result = new OperationResult<IEnumerable<MedicineType>>(model);
+
+                    return View(result);
+                }
+                else if (response.StatusCode == HttpStatusCode.BadRequest)
+                {
+                    var model = await response.Content.ReadAsAsync<IEnumerable<Error>>();
+                    var result = new OperationResult<IEnumerable<MedicineType>>(model);
+
+                    return View(result);
+                }
+
+                return Error();
+            }
+            catch
+            {
+                return Error();
+            }
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> AddMedicine(MedicineInputModel medicine)
+        {
+            string urlBuild = url;
+
+            try
+            {
+                var stringContent = new StringContent(JsonConvert.SerializeObject(medicine), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await new HttpClient().PostAsync(urlBuild, stringContent);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    var model = await response.Content.ReadAsAsync<Result<string>>();
+                    var result = new OperationResult<string>(model);
+
+                    return Json(new {type = "OK"});
+
+                }
+                else if (response.StatusCode == HttpStatusCode.BadRequest)
+                {
+                    var model = await response.Content.ReadAsAsync<IEnumerable<Error>>();
+                    var result = new OperationResult<string>(model);
+
+                    if (result.Errors.All(x => !string.IsNullOrEmpty(x.FieldName)))
+                    {
+                        return Json(new { type = "Validation", data = result.Errors });
+                    }
+                }
+
+                return Json(new { type = "Error" });
+            }
+            catch
+            {
+                return Json(new { type = "Error" });
             }
         }
 
---
+++ PharmaStore/PharmaStoreWeb/Helpers/GlobalHelpers.cs
@@ -0,0 +1,15 @@
+﻿namespace PharmaStoreWeb.Helpers
+{
+    public static class GlobalHelpers
+    {
+        public static string ParsePriceWithCurrency(string price, string currency)
+        {
+            decimal.TryParse(price, out var _price);
+            price = _price.ToString("# ##0.00").Replace('.', ',');
+            price = price.Trim();
+            price = price.Replace('.', ',');
+
+            return $"{price} {currency}".Replace('.', ',');
+        }
+    }
+}
